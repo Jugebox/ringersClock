97b52a416e83adb05955fbaaf91666e20a161353
Juhana Kuparinen <jmkupa@utu.fi>
2021-01-18 18:57:32 +0200

    Yksinkertainen serveri, joka voi ottaa vastaan viestejä clientiltä toteutettu!

--- src/main/java/fi/utu/tech/ringersClockServer/ClientThread.java
+++ src/main/java/fi/utu/tech/ringersClockServer/ClientThread.java
@@ -1,15 +1,14 @@
 package fi.utu.tech.ringersClockServer;
 
-import  java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.io.IOException;
 
 public class ClientThread implements Runnable {
 
     private Socket clientSocket;
-
-    public ClientThread() {
-        super();
-    }
 
     public ClientThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -17,11 +16,45 @@ public class ClientThread implements Runnable {
 
     public void run() {
         System.out.println("Accepted Client Address - " + clientSocket.getInetAddress().getHostName());
+        PrintWriter out = null;
+        BufferedReader in = null;
         try {
-            clientSocket.close();
-            System.out.println("...Stopped");
-        } catch (IOException e){
+
+            // get the outputstream of client
+            out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+
+            // get the inputstream of client
+            in = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                // writing the received message from
+                // client
+                System.out.printf(
+                        " Sent from the client: %s\n",
+                        line);
+                out.println(line);
+            }
+        }
+        catch (IOException e) {
             e.printStackTrace();
+        }
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                    clientSocket.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
